123fd6e29a19b116737a2febd10820112849744f
Matias Palmroth <matias.palmroth@helsinki.fi>
2025-11-23 22:05:08 +0200

    Update .coveragerc and tests

--- src/tests/encryption_test.py
+++ src/tests/encryption_test.py
@@ -1,18 +1,26 @@
+import os
 import unittest
-from encryption.key_manager import KeyManager
+
+from encryption.key_manager import get_key_from_password
 from encryption.encryptor import Encryptor
 from encryption.decryptor import Decryptor
 
 
 class TestEncryption(unittest.TestCase):
-    def test_encrypt_and_decrypt(self):
-        km = KeyManager("test_key.key")
-        key = km.generate_key()
-        enc = Encryptor(key)
-        dec = Decryptor(key)
+    def setUp(self):
+        password = "testi-testi-testi-testi"
+        key = get_key_from_password(password)
+        self.encryptor = Encryptor(key)
+        self.decryptor = Decryptor(key)
 
-        original_data = b"Hello world!"
-        encrypted = enc.fernet.encrypt(original_data)
-        decrypted = dec.fernet.decrypt(encrypted)
+    def test_passwords(self):
+        password = "salanatestisalasana"
+        key1 = get_key_from_password(password)
+        key2 = get_key_from_password(password)
+        self.assertEqual(key1, key2)
 
+    def test_encrypt_and_decrypt_memory(self):
+        original_data = b"Testi-data-testi-data-dii-daa-doo!"
+        encrypted = self.encryptor.fernet.encrypt(original_data)
+        decrypted = self.decryptor.fernet.decrypt(encrypted)
         self.assertEqual(decrypted, original_data)
